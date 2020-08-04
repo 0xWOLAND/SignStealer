a22eafdab34dd7f156b85b6faf9cb087a8fd4ed8
= <bhargav.annem@gmail.com>
2020-08-04 10:52:56 -0500

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Display from "./Components/Display"
+function App() {
+  return (
+    <div className="App">
+      <Display />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Display.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react"
+import "./Display.css"
+import Button from 'react-bootstrap/Button';
+
+export default class Display extends Component {
+        render(){
+                return <div id="main">
+                        <div id="letters"></div>
+                        <input />
+                </div>
+        }
+}
